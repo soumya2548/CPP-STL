79c72d8d70cec18d784e1cbedfae997d6810e4df
soumya2548 <128300429+soumya2548@users.noreply.github.com>
2023-07-22 19:31:19 +0530

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -8,7 +8,7 @@ int main(){
     vector<int>v3={2,4,65,2};//How to fill values in vector
 
     //Important operations
-    v0.push_back("sAYAn");//It fills the vector with a given anything from the end only
+    v0.push_back("CPP");//It fills the vector with a given anything from the end only
     v0.pop_back();//It pops out any data from the end
 
     //iterator loop
